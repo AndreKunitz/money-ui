5a96ba6cceee4bdb5f74abc35355592d6de792d8
Andre Kunitz <andrekunitz@gmail.com>
2019-02-19 09:52:15 -0300

    Adicionando menu intercambiável e ajustando Font Awesome

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,25 +7,19 @@ import { TableModule } from "primeng/table";
 import { TooltipModule } from "primeng/tooltip";
 
 import { AppComponent } from "./app.component";
-import { NavbarComponent } from './navbar/navbar.component';
-import { LancamentosPesquisaComponent } from './lancamentos-pesquisa/lancamentos-pesquisa.component';
+import { NavbarComponent } from "./navbar/navbar.component";
+import { LancamentosPesquisaComponent } from "./lancamentos-pesquisa/lancamentos-pesquisa.component";
 
 @NgModule({
-   declarations: [
-      AppComponent,
-      NavbarComponent,
-      LancamentosPesquisaComponent
-   ],
-   imports: [
-      BrowserModule,
-      InputTextModule,
-      ButtonModule,
-      TableModule,
-      TooltipModule
-   ],
-   providers: [],
-   bootstrap: [
-      AppComponent
-   ]
+  declarations: [AppComponent, NavbarComponent, LancamentosPesquisaComponent],
+  imports: [
+    BrowserModule,
+    InputTextModule,
+    ButtonModule,
+    TableModule,
+    TooltipModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
 export class AppModule {}
