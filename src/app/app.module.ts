13faec4feaf76e6246365928d538f6ae1aa18873
André Kunitz <andrekunitz@gmail.com>
2019-05-16 15:40:55 -0300

    Ajustando módulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,12 @@
-import { PessoaService } from './pessoas/pessoa.service';
-import { LancamentoService } from './lancamentos/lancamento.service';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule, LOCALE_ID } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
-import { registerLocaleData } from '@angular/common';
-import localePt from '@angular/common/locales/pt';
-
-import { ToastyModule } from 'ng2-toasty';
 
 import { CoreModule } from './core/core.module';
 import { LancamentosModule } from './lancamentos/lancamentos.module';
 import { PessoasModule } from './pessoas/pessoas.module';
-
-registerLocaleData(localePt);
 
 @NgModule({
   declarations: [AppComponent],
@@ -23,17 +15,11 @@ registerLocaleData(localePt);
     BrowserAnimationsModule,
     HttpClientModule,
 
-    ToastyModule.forRoot(),
-
     CoreModule,
     LancamentosModule,
     PessoasModule
   ],
-  providers: [
-    LancamentoService,
-    PessoaService,
-    { provide: LOCALE_ID, useValue: 'pt-BR' }
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,16 +1,26 @@
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
+
+import { ToastyModule } from 'ng2-toasty';
+
+import { ErrorHandlerService } from './error-handler.service';
+import { PessoaService } from './../pessoas/pessoa.service';
+import { LancamentoService } from './../lancamentos/lancamento.service';
+
+registerLocaleData(localePt);
 
 @NgModule({
-  declarations: [
-    NavbarComponent
-  ],
-  imports: [
-    CommonModule
-  ],
-  exports: [
-    NavbarComponent
+  declarations: [NavbarComponent],
+  imports: [CommonModule, ToastyModule.forRoot()],
+  exports: [NavbarComponent, ToastyModule],
+  providers: [
+    ErrorHandlerService,
+    LancamentoService,
+    PessoaService,
+    { provide: LOCALE_ID, useValue: 'pt-BR' }
   ]
 })
-export class CoreModule { }
+export class CoreModule {}
