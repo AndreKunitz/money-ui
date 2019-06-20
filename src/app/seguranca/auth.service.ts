3623a1f5141a2bf7269da0ae5b6081a204432e71
André Kunitz <andrekunitz@gmail.com>
2019-06-20 14:52:48 -0300

    Obtendo um novo access token

--- src/app/seguranca/auth.service.ts
+++ src/app/seguranca/auth.service.ts
@@ -3,12 +3,17 @@ import { Injectable } from '@angular/core';
 
 import { Observable } from 'rxjs';
 import { JwtHelperService } from '@auth0/angular-jwt';
+import { ErrorHandlerService } from '../core/error-handler.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   private oauthToeknUrl = 'http://localhost:8080/oauth/token';
+  private headers = new HttpHeaders({
+    'Content-Type': 'application/x-www-form-urlencoded',
+    Authorization: 'Basic YW5ndWxhcjphbmd1bGFy'
+  });
   jwtPayload: any;
 
   constructor(private http: HttpClient, private jwtHelper: JwtHelperService) {
@@ -16,14 +21,26 @@ export class AuthService {
   }
 
   login(usuario: string, senha: string): Observable<void> {
-    const headers = new HttpHeaders({
-      'Content-Type': 'application/x-www-form-urlencoded',
-      Authorization: 'Basic YW5ndWxhcjphbmd1bGFy'
-    });
-
     const body = `username=${usuario}&password=${senha}&grant_type=password`;
 
-    return this.http.post<void>(this.oauthToeknUrl, body, { headers });
+    return this.http.post<void>(this.oauthToeknUrl, body, {
+      headers: this.headers,
+      withCredentials: true
+    });
+  }
+
+  obterNovoAccessToken() {
+    const body = `grant_type=refresh_token`;
+
+    return this.http
+      .post<void>(this.oauthToeknUrl, body, {
+        headers: this.headers,
+        withCredentials: true
+      })
+      .subscribe(resp => {
+        this.armazernarToken(resp['access_token']);
+        console.log('Novo access token');
+      });
   }
 
   armazernarToken(token: string) {
