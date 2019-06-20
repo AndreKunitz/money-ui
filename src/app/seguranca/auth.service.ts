7d0df58b8a18140f419b0b38b3ac92d35567151a
André Kunitz <andrekunitz@gmail.com>
2019-06-20 14:05:39 -0300

    Exibindo o menu do sistema conforme permissões do usuário

--- src/app/seguranca/auth.service.ts
+++ src/app/seguranca/auth.service.ts
@@ -38,4 +38,8 @@ export class AuthService {
       this.armazernarToken(token);
     }
   }
+
+  temPermissao(permissao: string): boolean {
+    return this.jwtPayload && this.jwtPayload.authorities.includes(permissao);
+  }
 }
