69bf56149a98cf3ea6a03ea260aa850aeb749177
André Kunitz <andrekunitz@gmail.com>
2019-06-25 10:21:38 -0300

    Ajustando RegExp pra lista de domínios e rotas nas configurações de Environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   apiUrl: 'https://andremoney-ui.herokuapp.com',
 
-  tokenWhitelistedDomains: [/andremoney-ui.herokuapp.com/],
-  tokenBlacklistedRoutes: [/\/oauth\/token/]
+  tokenWhitelistedDomains: [new RegExp('andremoney-ui.herokuapp.com')],
+  tokenBlacklistedRoutes: [new RegExp('/oauth/token')]
 };
